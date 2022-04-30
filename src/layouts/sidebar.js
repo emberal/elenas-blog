e7cc3106ce070bb26e3d09b1fab59a06c1825d63
Martin Berg Alstad <600878@stud.hvl.no>
2022-04-30 23:53:27 +0200

    BackToTop button is hidden while at the top of the page, and animate on push instead of hover

--- src/layouts/sidebar.js
+++ src/layouts/sidebar.js
@@ -4,16 +4,35 @@ import {StaticImage} from "gatsby-plugin-image";
 import {sidebar, hidden, scrollBackUpButtonPos} from "../stylesheets/media.module.css";
 import {scrollBackUpButton} from "../stylesheets/sidebar.module.css"
 import {iconStyle} from "../stylesheets/page.module.css";
+import {useEffect, useState} from "react";
 
 function scrollToTop() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chromium, Firefox, IE and Opera
 }
 
-const Sidebar = ({color}) => { //TODO only show scrollButton after scrolling down
+const Sidebar = ({color}) => {
+    const [isTop, setIsTop] = useState(false);
+    useEffect(() => {
+        const handleScroll = () => {
+            const show = window.scrollY > 20;
+            if (isTop !== show) {
+                setIsTop(show)
+            }
+        }
+        document.addEventListener('scroll', handleScroll)
+        return () => {
+            document.removeEventListener('scroll', handleScroll)
+        }
+    }, [])
+
     return(
         <div className={classNames(sidebar, color)}>
-            <button className={classNames(scrollBackUpButton, scrollBackUpButtonPos)} onClick={scrollToTop} title={"Back to the top"}>
+            <button
+                className={(isTop) ?
+                    classNames(scrollBackUpButton, scrollBackUpButtonPos) :
+                    classNames(scrollBackUpButton, scrollBackUpButtonPos, hidden)}
+                onClick={scrollToTop} title={"Back to the top"}>
                 <StaticImage className={iconStyle} src={"../images/icons8-up-squared-48.png"} alt={"Arrow pointing up"}/>
                 <p className={hidden}>Scroll back to the top</p>
             </button>
