18907adcdc95b39508816937493dc31aaafc7352
Martin Berg Alstad <600878@stud.hvl.no>
2022-05-03 23:59:19 +0200

    Experimented with grid in gallery

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -6,7 +6,6 @@ import {imageContainer} from "../stylesheets/page.module.css"
 import {galleryPageColor} from "../stylesheets/colors.module.css"
 
 const imageStyle = {
-    width: "fit-content", height: "fit-content",
     display: "flex",
     justifyContent: "center",
     position: "relative",
@@ -14,9 +13,12 @@ const imageStyle = {
 }
 const galleryContainer = {
     overflow: "auto",
-    display: "flex",
-    flexFlow: "row wrap",
-    justifyContent: "space-around",
+    display: "grid",
+    gridTemplateColumns: "repeat(4, 1fr)",
+    gridAutoRows: "auto",
+    gridGap: "1em",
+    justifyContent: "center",
+    paddingLeft: "2.5%", paddingRight: "2.5%",
 }
 const dataStyle = {
     paddingLeft: "10px",
@@ -26,29 +28,45 @@ const dataStyle = {
 }
 
 const Gallery = ({data}) => {
+
+    let pics = [];
+    const blogPost = data.allContentfulBlogPost;
+    for (let i = 0; i < blogPost.totalCount; i++) {
+        let j = 0;
+        while (blogPost.nodes[i].pictures[j] !== undefined) {
+            const pic = {
+                title: blogPost.nodes[i].title,
+                slug: blogPost.nodes[i].slug,
+                createdAt: blogPost.nodes[i].createdAt,
+                timeToRead: blogPost.nodes[i].body.childMarkdownRemark.timeToRead,
+                pic: blogPost.nodes[i].pictures[j].gatsbyImageData,
+                picAlt: blogPost.nodes[i].pictures[j].description,
+                picId: blogPost.nodes[i].pictures[j].id,
+            }
+            pics.push(pic);
+            j++;
+        }
+
+    }
+
     return( //TODO make all images from the same post in a swipeable container, with left and right buttons on the side
         <Layout title={"Gallery"} homePageColor={galleryPageColor} children={
             <div style={galleryContainer}>
                 {
-                    data.allContentfulBlogPost.nodes.map( node => ( //Iterates through blogposts
-                        <div key={node.id}>
-                            {
-                                node.pictures.map( picture => ( //Iterates through pictures for each blogpost
-                                    <div className={imageContainer} key={picture.id}>
-                                        <Link to={"../blog/" + node.slug}>
-                                            <GatsbyImage
-                                                style={imageStyle}
-                                                image={getImage(picture.gatsbyImageData)}
-                                                alt={picture.description}
-                                            />
-                                            <h3 style={dataStyle}>{node.title}</h3>
-                                        </Link>
-                                        <p></p>
-                                        <p style={dataStyle}>Published: {node.createdAt}</p>
-                                        <p style={dataStyle}>{node.body.childMarkdownRemark.timeToRead} minutes to read</p>
-                                    </div>
-                                ))
-                            }
+                    pics.map( pic => ( //Iterates through pictures for each blogpost
+                        <div className={imageContainer} key={pic.picId}>
+                            <Link to={"../blog/" + pic.slug}>
+                                <GatsbyImage
+                                    style={imageStyle}
+                                    image={getImage(pic.pic)}
+                                    alt={pic.picAlt}
+                                />
+                                <h3 style={dataStyle}>{pic.title}</h3>
+                            </Link>
+                            <p></p>
+                            <p style={dataStyle}>Published: {pic.createdAt}</p>
+                            <p style={dataStyle}>{pic.timeToRead} minutes to read</p>
+                            <p></p>
                         </div>
                     ))
                 }
@@ -61,6 +79,7 @@ const Gallery = ({data}) => {
 export const query = graphql `
 query {
   allContentfulBlogPost(sort: {fields: createdAt, order: DESC}) {
+    totalCount
     nodes {
       title
       createdAt(formatString: "Do MMMM YYYY, H:mm")
