bfd1d7e741500264ef58ef5022a44619646ab7a6
Martin Berg Alstad <600878@stud.hvl.no>
2022-05-07 15:31:01 +0200

    Fixed images won't load on touch screens after refresh, updated dependencies

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -62,82 +62,88 @@ const Gallery = ({data}) => {
         setPicObj(null);
     }
 
-    return( //TODO Keyboard listeners for onClick
+    return ( //TODO Keyboard listeners for onClick
         <Layout title={"Gallery"} homePageColor={galleryPageColor} children={
-            (pics.length === 0) ? <span className={emptyPageStyle}>There are currently no posts with pictures :(</span> : (
-                <div className={classNames(galleryContainer, galleryGrid)}>
-                    {
-                        pics.map( pic => (
-                            <div className={imageContainer} key={pic.id}>
-                                {console.log(pic)}
-                                {
-                                    (isTouch) ? ( //Touch screen
-                                        <div>
-                                            <GatsbyImage
-                                                role={"img"}
-                                                onClick={() => openModal(pic)}
-                                                style={imageStyle}
-                                                image={getImage(pic.img)}
-                                                alt={pic.imgAlt}
-                                            />
-                                            <Modal
-                                                isOpen={modalIsOpen}
-                                                onRequestClose={closeModal}
-                                                contentLabel={"Image"}
-                                                className={content}
-                                                overlayClassName={overlay}
-                                                shouldCloseOnOverlayClick={true}
-                                                shouldCloseOnEsc={true}
-                                                bodyOpenClassName={ReactModal__BodyOpen}>
-                                                <div>
-                                                    {(picObj === null) ? null :
-                                                        (
-                                                            <div>
-                                                                <StaticImage
-                                                                    role={"img"}
-                                                                    style={closeButtonStyle}
-                                                                    onClick={closeModal}
-                                                                    src={"../images/icons8-close-window-48.png"}
-                                                                    alt={"Close modal button"}/>
-                                                                <GatsbyImage style={imageStyle} alt={picObj.imgAlt} image={getImage(picObj.img)}/>
-                                                                <div className={galleryOverlayDataStyle}>
-                                                                    <Link to={"../blog/" + picObj.slug}>
-                                                                        <h3>{picObj.title}</h3>
-                                                                    </Link>
-                                                                    <BlogData createdAt={picObj.createdAt} timeToRead={picObj.timeToRead}/>
-                                                                    <p></p>
-                                                                </div>
+            (pics.length === 0) ?
+                <span className={emptyPageStyle}>There are currently no posts with pictures :(</span> : (
+                    <div className={classNames(galleryContainer, galleryGrid)}>
+                        {
+                            (isTouch) ? (
+                                pics.map(pic => ( //Touch screen
+                                    <article className={imageContainer} key={pic.id}> {/*Should be <div> but images won't load after page reloads*/}
+                                        <GatsbyImage
+                                            role={"img"}
+                                            onClick={() => openModal(pic)}
+                                            style={imageStyle}
+                                            image={getImage(pic.img)}
+                                            alt={pic.imgAlt}
+                                        />
+                                        <Modal
+                                            isOpen={modalIsOpen}
+                                            onRequestClose={closeModal}
+                                            contentLabel={"Image"}
+                                            className={content}
+                                            overlayClassName={overlay}
+                                            shouldCloseOnOverlayClick={true}
+                                            shouldCloseOnEsc={true}
+                                            bodyOpenClassName={ReactModal__BodyOpen}>
+                                            <div>
+                                                {(picObj === null) ? null :
+                                                    (
+                                                        <>
+                                                            <StaticImage
+                                                                role={"img"}
+                                                                style={closeButtonStyle}
+                                                                onClick={closeModal}
+                                                                src={"../images/icons8-close-window-48.png"}
+                                                                alt={"Close modal button"}/>
+                                                            <GatsbyImage style={imageStyle} alt={picObj.imgAlt}
+                                                                         image={getImage(picObj.img)}/>
+                                                            <div className={galleryOverlayDataStyle}>
+                                                                <Link to={"../blog/" + picObj.slug}>
+                                                                    <h3>{picObj.title}</h3>
+                                                                </Link>
+                                                                <BlogData createdAt={picObj.createdAt}
+                                                                          timeToRead={picObj.timeToRead}/>
+                                                                <p></p>
                                                             </div>
-                                                        )
-                                                    }
+                                                        </>
+                                                    )
+                                                }
+                                            </div>
+                                        </Modal>
+                                    </article>
+                                ))
+                            ) : (
+                                <>
+                                    { //Not touch screen
+                                        pics.map(pic => (
+                                            <div className={imageContainer} key={pic.id}>
+                                                <Link to={"../blog/" + pic.slug}>
+                                                    <GatsbyImage
+                                                        style={imageStyle}
+                                                        image={getImage(pic.img)}
+                                                        alt={pic.imgAlt}
+                                                    />
+                                                </Link>
+                                                <div className={galleryDataStyle}>
+                                                    <Link style={{color: "white"}} to={"../blog/" + pic.slug}>
+                                                        <h3>{pic.title}</h3>
+                                                    </Link>
+                                                    <BlogData createdAt={pic.createdAt} timeToRead={pic.timeToRead}/>
+                                                    <p></p>
                                                 </div>
-                                            </Modal>
-                                        </div>
-                                    ) : ( //Not touch screen
-                                        <Link to={"../blog/" + pic.slug}>
-                                            <GatsbyImage
-                                                style={imageStyle}
-                                                image={getImage(pic.img)}
-                                                alt={pic.imgAlt}
-                                            />
-                                        </Link>
-                                    )
-                                }
-                                <div className={galleryDataStyle}>
-                                    <Link style={{color: "white"}} to={"../blog/" + pic.slug}>
-                                        <h3>{pic.title}</h3>
-                                    </Link>
-                                    <BlogData createdAt={pic.createdAt} timeToRead={pic.timeToRead}/>
-                                    <p></p>
-                                </div>
-                            </div>
-                        ))
-                    }
-                </div>
-            )
+                                            </div>
+                                        ))
+                                    }
+                                </>
+                            ) //End isTouch
+                        }
+                    </div>
+                )
         }
-        description={"A gallery of delicious food."}/> //TODO better description
-    )
+                description={"A gallery of delicious food."}/> //TODO better description
+    );
 }
 
 export const query = graphql `
